ef41a63cbe1360b7de15c44909184b5d17407645
ilja <ilja.tarasovs@gmail.com>
2024-12-21 22:15:42 +0200

    wip

--- application/infrastructure/client/product-nutrition/src/main/kotlin/com.example.infrastructure.client.productnutrition/ProductNutritionClientImpl.kt
+++ application/infrastructure/client/product-nutrition/src/main/kotlin/com.example.infrastructure.client.productnutrition/ProductNutritionClientImpl.kt
@@ -1,21 +1,42 @@
 package com.example.infrastructure.client.productnutrition
 
-import com.example.domain.products.Product
 import com.example.domain.products.Product as ProductDomain
 import com.example.domain.products.ProductNutritionClient
 import com.example.infrastructure.client.productnutrition.api.ProductNutritionApi
 import com.example.infrastructure.client.productnutrition.invoker.ApiClient
+import com.example.infrastructure.client.productnutrition.invoker.ApiResponseDecoder
+import feign.form.FormEncoder
+import feign.jackson.JacksonEncoder
+import feign.okhttp.OkHttpClient
+import feign.slf4j.Slf4jLogger
 
-class ProductNutritionClientImpl(
-) : ProductNutritionClient {
-    override fun findById(id: String): Product {
-        val apiClient = ApiClient();
-        apiClient.basePath = "http://localhost:8081/product-nutrition"
-        val client = apiClient.buildClient(ProductNutritionApi::class.java)
+class ProductNutritionClientImpl : ProductNutritionClient {
 
-        val productNutrition = client.getProductById("1")
+    override fun findById(id: String): ProductDomain {
+        val apiClient = ApiClient()
+        apiClient.basePath = "http://localhost:8081/product-nutrition"  // must be provided by configuration
+        val feignClient = apiClient.feignBuilder
+            .client(OkHttpClient()) // perhaps something else can be used
+            .logger(Slf4jLogger())  // perhaps something else can be used
+            .encoder(               // perhaps something else can be used
+                FormEncoder(
+                    JacksonEncoder(
+                        apiClient.objectMapper  // Potentially objectMapper needs to be configured
+                    )
+                )
+            )
+            .decoder(               // perhaps something else can be used
+                ApiResponseDecoder(
+                    apiClient.objectMapper
+                )
+            )
+//            .retryer()            // perhaps something else can be used
+//            .errorDecoder()       // perhaps something else can be used
+            .target(ProductNutritionApi::class.java, apiClient.basePath)
+
+        val productNutrition = feignClient.getProductById("1")
         println(productNutrition)
 
-        return ProductDomain(title = "", count = 1)
+        return ProductDomain(title = "hola", count = 1)
     }
 }
