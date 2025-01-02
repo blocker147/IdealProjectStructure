d8aa3e545826303f9fd5ab4dd68e9dc478abc1a5
blocker147 <ilja.tarasovs@gmail.com>
2025-01-02 22:52:11 +0200

    wip

--- application/spring/security/src/test/kotlin/com/example/spring/security/jwt/JWTServiceTest.kt
+++ application/spring/security/src/test/kotlin/com/example/spring/security/jwt/JWTServiceTest.kt
@@ -1,11 +1,9 @@
 package com.example.spring.security.jwt
 
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.security.Keys
-import io.jsonwebtoken.impl.FixedClock
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.Clock
 import java.time.Instant
@@ -15,33 +13,35 @@ class JWTServiceTest {
     private val jwtSigningKey = "ZXcXQZHDcogtSJV1oZhG4On5xn4rmCcLp3Qp5nwFKh79kRYg38St7yvSyBCfdaMrIeNWK5bwHmblRehRGAvg9nTXIVL7W0AHQhlrheV3EGpzZiaifnifIKeyr8ZyvCqjzVzUFnZMb79uIlHp82DoMUR867cVuHRv7qlpXdPcVDrB0dRQ1IIuQYhIJLRQ3IFkmfgInIITczZRsTceHu0OhzTSmicLL9CSRPfZvpEsPO4Zta5Ewf2dot3Dh5WrHawF"
     private val instant = Instant.parse("1997-01-01T00:00:00Z")
     private val clockFake = Clock.fixed(instant, Clock.systemUTC().zone)
-    private val target = JWTService(jwtSigningKey, clockFake)
+    private lateinit var target: JWTService
+
+    @BeforeEach
+    fun setup() {
+        target = JWTService(jwtSigningKey, clockFake)
+    }
+
     private val username = "username"
-    private val secretKey = Keys.hmacShaKeyFor(jwtSigningKey.toByteArray())
-    private val jsonWebTokenClock: io.jsonwebtoken.Clock = FixedClock(clockFake.millis())
 
     @Test
     fun `when generate access token - then return valid token`() {
-        val (token, _) = target.generateToken(username, JWTService.TokenType.ACCESS_TOKEN)
+        val (token, expiresAfter) = target.generateToken(username, JWTService.TokenType.ACCESS_TOKEN)
 
-        val (sub, iat, exp) = getPayload(token)
+        val actual = target.verifyToken(token, JWTService.TokenType.ACCESS_TOKEN)
 
-        sub.shouldNotBeNull()
-        iat.shouldNotBeNull()
-        exp.shouldNotBeNull()
-        exp shouldBe clockFake.millis() + JWTService.JWT_ACCESS_TOKEN_EXPIRATION
+        actual.shouldNotBeNull()
+        actual shouldBe username
+        expiresAfter shouldBe JWTService.JWT_ACCESS_TOKEN_EXPIRATION
     }
 
     @Test
     fun `when generate refresh token - then return valid token`() {
-        val (token, _) = target.generateToken(username, JWTService.TokenType.REFRESH_TOKEN)
+        val (token, expiresAfter) = target.generateToken(username, JWTService.TokenType.REFRESH_TOKEN)
 
-        val (sub, iat, exp) = getPayload(token)
+        val actual = target.verifyToken(token, JWTService.TokenType.REFRESH_TOKEN)
 
-        sub.shouldNotBeNull()
-        iat.shouldNotBeNull()
-        exp.shouldNotBeNull()
-        exp shouldBe clockFake.millis() + JWTService.JWT_REFRESH_TOKEN_EXPIRATION
+        actual.shouldNotBeNull()
+        actual shouldBe username
+        expiresAfter shouldBe JWTService.JWT_REFRESH_TOKEN_EXPIRATION
     }
 
     @Test
@@ -130,24 +130,4 @@ class JWTServiceTest {
 
         actual.shouldBeNull()
     }
-
-    private fun getPayload(token: String): Payload {
-        val claims = Jwts.parser()
-            .clock(jsonWebTokenClock)
-            .verifyWith(secretKey)
-            .build()
-            .parseSignedClaims(token)
-            .payload
-        return Payload(
-            sub = claims.subject,
-            iat = claims.issuedAt.time,
-            exp = claims.expiration.time,
-        )
-    }
-
-    private data class Payload(
-        val sub: String,
-        val iat: Long,
-        val exp: Long,
-    )
 }
